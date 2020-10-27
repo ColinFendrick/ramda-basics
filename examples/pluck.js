7a3df053d0b7b1463009ed8de3f93c4bb2bd789a
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-27 18:45:35 -0400

    Another pluck example

--- examples/pluck.js
+++ examples/pluck.js
@@ -20,3 +20,7 @@ console.log(`
   plucing ages: ${pluck('age', users)}
   plucking names: ${pluck('name', users)}
 `);
+
+// In the following example we will work with the formed list
+const maxAge = Math.max(...pluck('age', users));
+console.log(`The oldest person is ${maxAge} years old.`);
