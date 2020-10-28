73b00e8cb61afd392907d8537d6e3d08ba1c84a3
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-28 05:47:28 -0400

    Final

--- index.js
+++ index.js
@@ -11,28 +11,58 @@ export const users = [
 	{ name: 'Robert', age: 72 }
 ];
 
+export const members = [
+	{ name: 'John', city: 'London', born: '2001-04-01' },
+	{ name: 'Lenny', city: 'New York', born: '1997-12-11' },
+	{ name: 'Andrew', city: 'Boston', born: '1987-02-22' },
+	{ name: 'Peter', city: 'Prague', born: '1936-03-24' },
+	{ name: 'Anna', city: 'Bratislava', born: '1973-11-18' },
+	{ name: 'Albert', city: 'Bratislava', born: '1940-12-11' },
+	{ name: 'Adam', city: 'Trnava', born: '1983-12-01' },
+	{ name: 'Robert', city: 'Bratislava', born: '1935-05-15' },
+	{ name: 'Robert', city: 'Prague', born: '1998-03-14' }
+];
+
+export const students = [
+	{ name: 'Adam', score: 84 },
+	{ name: 'Eddy', score: 58 },
+	{ name: 'Peter', score: 69 },
+	{ name: 'Roman', score: 93 },
+	{ name: 'Jane', score: 56 },
+	{ name: 'Lucy', score: 76 },
+	{ name: 'Jack', score: 88 }
+];
+
 export const {
 	add,
 	apply,
 	call,
-	contains,
 	comparator,
-	flip,
+	contains,
+	equals,
+	filter,
 	find,
 	findLast,
+	flip,
+	groupBy,
 	gt,
 	head,
 	init,
 	last,
 	lt,
 	map,
-	prop,
+	partition,
 	pluck,
+	prop,
+	reduce,
+	reject,
 	repeat,
 	reverse,
-	splitEvery,
 	sort,
 	sortBy,
+	splitEvery,
+	startsWith,
 	subtract,
-	tail
+	tail,
+	where
 } = R;
